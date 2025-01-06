bb28d75fbde4558fe09b597823bed00aa8a63bc0
Michał Kędrzyński <kedrzu@gmail.com>
2025-01-06 15:38:45 +0100

    chore: setting up nx

--- packages/aws-cdk/src/getSdkProvider.ts
+++
@@ -1,7 +0,0 @@
-import { SdkProvider } from 'aws-cdk/lib/api/aws-auth/index.js';
-
-export function getSdkProvider() {
-    return SdkProvider.withAwsCliCompatibleDefaults({});
-}
-
-export type { SdkProvider } from 'aws-cdk/lib/api/aws-auth/index.js';
--- packages/aws-cdk/src/index.ts
+++
@@ -1,3 +0,0 @@
-export * from './App.js';
-export * from './Stack.js';
-export * from './getSdkProvider.js';
